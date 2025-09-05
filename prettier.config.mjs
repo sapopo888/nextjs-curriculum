975092de469ac941cfc54d4f324da2c4b7ff2b42
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-05 12:14:33 +0900

    prettierの設定、コード整形実行

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  singleQuote: true, // 文字列はシングルクオートを使用
+  semi: false, // 行末にセミコロンをつけない
+  tabWidth: 2, // インデントはスペース２つ
+  bracketSpacing: true, // オブジェクトの波括弧の間にスペースを入れる
+}
+
+export default config
